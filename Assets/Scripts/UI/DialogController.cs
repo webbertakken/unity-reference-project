33221cd1f1430b1492898de859dc81bbfb404e14
Webber <webber@takken.io>
2021-05-13 00:23:30 +0200

    Add dialog giver

--- Assets/Scripts/UI/DialogController.cs
+++ Assets/Scripts/UI/DialogController.cs
@@ -1,55 +1,53 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Text;
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class DialogController : MonoBehaviour
+namespace UI
 {
-  [SerializeField] TextAsset _dialog;
-  [SerializeField] TMP_Text _storyText;
-  [SerializeField] Button[] _choiceButtons;
+  public class DialogController : MonoBehaviour
+  {
+    [SerializeField] TMP_Text _storyText;
+    [SerializeField] Button[] _choiceButtons;
 
-  Story _story;
+    Story _story;
 
-  void Awake() { }
+    void Awake() { }
 
-  [ContextMenu("Start Dialog")]
-  void StartDialog() {
-    _story = new Story(_dialog.text);
-    RefreshView();
-  }
-
-  void RefreshView() {
-    StringBuilder storyTestBuilder = new StringBuilder();
-
-    while (_story.canContinue) {
-      storyTestBuilder.AppendLine(_story.Continue());
+    [ContextMenu("Start Dialog")]
+    public void StartDialog(TextAsset dialog) {
+      _story = new Story(dialog.text);
+      RefreshView();
     }
 
-    _storyText.SetText(storyTestBuilder);
+    void RefreshView() {
+      StringBuilder storyTestBuilder = new StringBuilder();
 
-    for (int i = 0; i < _choiceButtons.Length; i++) {
-      var button = _choiceButtons[i];
-      var isActive = i < _story.currentChoices.Count;
+      while (_story.canContinue) {
+        storyTestBuilder.AppendLine(_story.Continue());
+      }
 
-      button.gameObject.SetActive(isActive);
-      button.onClick.RemoveAllListeners();
+      _storyText.SetText(storyTestBuilder);
 
-      if (!isActive) continue;
+      for (int i = 0; i < _choiceButtons.Length; i++) {
+        var button = _choiceButtons[i];
+        var isActive = i < _story.currentChoices.Count;
 
-      var choice = _story.currentChoices[i];
-      button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
-      button.onClick.AddListener(
-        () => {
-          _story.ChooseChoiceIndex(choice.index);
-          RefreshView();
-        }
-      );
+        button.gameObject.SetActive(isActive);
+        button.onClick.RemoveAllListeners();
+
+        if (!isActive) continue;
+
+        var choice = _story.currentChoices[i];
+        button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+        button.onClick.AddListener(
+          () => {
+            _story.ChooseChoiceIndex(choice.index);
+            RefreshView();
+          }
+        );
+      }
     }
   }
 }
