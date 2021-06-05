8d19d7574b099f90921d277b34b4950fa2820270
Webber <webber@takken.io>
2021-06-05 22:18:43 +0200

    Add gameflags

---
+++ Assets/Scripts/Flags/IntegerGameFlag.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Flags
+{
+  [CreateAssetMenu(menuName = "Flag (integer)", order = 52)]
+  public class IntegerGameFlag : GameFlag<int>
+  {
+    public void Add(int value) {
+      Value += value;
+      SendChanged();
+    }
+
+    public void Subtract(int value) {
+      Value -= value;
+      SendChanged();
+    }
+  }
+}
